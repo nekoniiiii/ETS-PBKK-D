75e8e1e8f9e60f40fa4c39f982e887a3efa689ea
Neisa <72701806+nekoniiiii@users.noreply.github.com>
2023-04-05 15:38:52 +0700

    init commit

---
+++ ETS_CurrencyConverter/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace ETS_CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem1 = comboBox1.SelectedItem;
+            var selectedItem2 = comboBox2.SelectedItem;
+            if (selectedItem1.ToString() == "USD" && selectedItem2.ToString() == "USD")
+            {
+                textBox2.Text = textBox1.Text;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        /*
+        private void InputBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            input = InputBox.Text;
+        }
+        */
+    }
+}
